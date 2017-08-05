32aa21b0e916d8da2014ac4a88e16abc7017d973
Adam <adam@adamharpur.com>
2017-08-05 09:44:22 -0500

    added logic to functions

--- app.js
+++ app.js
@@ -6,16 +6,24 @@ var config = {
 // poker hand evaluator
 hands = [ '8C TS KC 9H 4S', '7D 2S 5D 3S AC', '8C AD 8D AC 9C', '7C 5H 8D TD KS' ];
 
-// looks ath the suits and checks for a flush
-var suits = [];
-hands.forEach( function ( hand, pos ) {
-  for ( i = 1; i < hand.length; i += config.space_cards ) {
-    suits.push( hand[ i ] );
-  }
-} );
+suits = getSuits( hands );
+getHandsSuits( suits );
 
-// looks at the suits in chunks of five or hand length
-for ( i = 0; i < suits.length; i += config.hand_len ) {
-  var suit_hand = suits.slice( i, i + config.hand_len );
-  console.log( suit_hand );
+function getSuits( data ) {
+  var suits = [];
+  hands.forEach( function ( hand, pos ) {
+    for ( i = 1; i < hand.length; i += config.space_cards ) {
+      suits.push( hand[ i ] );
+    }
+  } );
+  return suits;
+} // looks at the suits and checks for a flush
+
+function getHandsSuits( suits ) {
+
+  // looks at the suits in chunks of five or hand length
+  for ( i = 0; i < suits.length; i += config.hand_len ) {
+    var suit_hand = suits.slice( i, i + config.hand_len );
+    console.log( suit_hand );
+  }
 }

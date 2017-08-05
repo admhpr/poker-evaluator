f9c8865ff8c9dcdca72d32e8befb09800eb5bb64
Adam <adam@adamharpur.com>
2017-08-05 09:18:39 -0500

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+var config = {
+  hand_len: 5,
+  space_cards: 3
+};
+
+// poker hand evaluator
+hands = [ '8C TS KC 9H 4S', '7D 2S 5D 3S AC', '8C AD 8D AC 9C', '7C 5H 8D TD KS' ];
+
+// looks ath the suits and checks for a flush
+var suits = [];
+hands.forEach( function ( hand, pos ) {
+  for ( i = 1; i < hand.length; i += config.space_cards ) {
+    suits.push( hand[ i ] );
+  }
+} );
+
+// looks at the suits in chunks of five or hand length
+for ( i = 0; i < suits.length; i += config.hand_len ) {
+  var suit_hand = suits.slice( i, i + config.hand_len );
+  console.log( suit_hand );
+}

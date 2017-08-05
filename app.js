df6d519299b6140ba3d8cc1021d513e6e6e5d79e
Adam <adam@adamharpur.com>
2017-08-05 10:38:14 -0500

    tidy up

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 var config = {
   hand_len: 5,
-  space_cards: 3
+  space_cards: 3,
+  start_pos_values: 0,
+  start_pos_suits: 1
 };
 
 // poker hand evaluator
 hands = [ '8C TS KC 9H 4S', '7D 2S 5D 3S AC', '8C AD 8D AC 9C', '7C 5H 8D TD KS' ];
 
-suits = getSuits( hands );
+suits = getData( hands, config.start_pos_suits );
+values = getData( hands, config.start_pos_values );
+console.log( values );
 isFlush( suits );
 
-function getSuits( data ) {
-  var suits = [];
+
+function getData( hands, start_pos ) {
+  var data = [];
   hands.forEach( function ( hand, pos ) {
-    for ( i = 1; i < hand.length; i += config.space_cards ) {
-      suits.push( hand[ i ] );
+    for ( i = start_pos; i < hand.length; i += config.space_cards ) {
+      data.push( hand[ i ] );
     }
   } );
-  return suits;
-} // looks at the suits and checks for a flush
+  return data;
+} // separates suits from their values
 
 function isFlush( suits ) {
-
-  // looks at the suits in chunks of five or hand length
+  // looks at the suits in chunks that are the hand length
   for ( i = 0; i < suits.length; i += config.hand_len ) {
     var suit_hand = suits.slice( i, i + config.hand_len );
     console.log( suit_hand );
+    // TODO 8/5/17 add logic to determine suit
   }
-}
+} // looks at the suits and checks for a flush

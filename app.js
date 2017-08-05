ccbda13fc188e4d149c67a55f29005f91b4a0994
Adam <adam@adamharpur.com>
2017-08-05 11:11:50 -0500

    tidy up

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ hands = [ '8C TS KC 9H 4S', '7D 2S 5D 3S AC', '8C AD 8D AC 9C', '7C 5H 8D TD KS'
 
 suits = getData( hands, config.start_pos_suits );
 values = getData( hands, config.start_pos_values );
-console.log( values );
-isFlush( suits );
+chunkHands( suits );
+chunkHands( values );
 
 
 function getData( hands, start_pos ) {
@@ -24,11 +24,11 @@ function getData( hands, start_pos ) {
   return data;
 } // separates suits from their values
 
-function isFlush( suits ) {
-  // looks at the suits in chunks that are the hand length
-  for ( i = 0; i < suits.length; i += config.hand_len ) {
-    var suit_hand = suits.slice( i, i + config.hand_len );
-    console.log( suit_hand );
-    // TODO 8/5/17 add logic to determine suit
+// looks at the suits in chunks that are the hand length
+function chunkHands( cards ) {
+  for ( i = 0; i < cards.length; i += config.hand_len ) {
+    var card_hand = cards.slice( i, i + config.hand_len );
+    console.log( card_hand );
+    // TODO 8/5/17 add logic to determine suit create a separate function
   }
 } // looks at the suits and checks for a flush

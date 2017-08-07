8bdb7ccae0f3986fd1b504d277fb3d4d9c8f807f
Adam <adam@adamharpur.com>
2017-08-06 21:28:56 -0500

    added function sequence

--- app.js
+++ app.js
@@ -21,20 +21,26 @@ var ranks = {
 };
 
 // data
-hands = [ '8C 8S 8H 8C TS', '8C TS KC 8H 4S', '7D 2S 5D 3S AC', '8C AD 8D AC 9C', '7C 5H 8D TD KS' ];
+hands = [ '8C 8S 8H 8C TS', '8C TC KC 8C 4C', '7D 2S 5D 3S AC', '8C AD 8D AC 9C', '7C 5H 8D TD KS' ];
 
 // poker hand evaluator
 ( function () {
-
   /***************************************************
                      Prepare Data
   ****************************************************/
   suits = chunkHands( getData( hands, config.start_pos_suits ) );
   values = chunkHands( eNum( getData( hands, config.start_pos_values ) ) );
+  handData = [];
 
   console.log( values );
   console.log( suits );
-  console.log( findMatches( values[ 0 ] ) );
+  console.log( values.length );
+
+  values.forEach( function ( hand, pos ) {
+    getHand( suits[ pos ], values[ pos ] );
+  } );
+
+  console.log( handData );
 
   function getData( hands, start_pos ) {
     var data = [];
@@ -53,12 +59,11 @@ hands = [ '8C 8S 8H 8C TS', '8C TS KC 8H 4S', '7D 2S 5D 3S AC', '8C AD 8D AC 9C'
       var card_hand = cards.slice( i, i + config.hand_len );
       card_hand.sort( ( a, b ) => a - b );
       handValues.push( card_hand );
-      // TODO 8/5/17 add logic to determine suit create a separate function
     }
     return handValues;
   }
 
-  // enumerates the face cards to make sorting easier //TODO 8/6/17 Ace may cause problems with straights
+  // enumerates the face cards to make sorting easier
   function eNum( values ) {
     r = /[AKQJT]/g;
     num = [];
@@ -83,30 +88,64 @@ hands = [ '8C 8S 8H 8C TS', '8C TS KC 8H 4S', '7D 2S 5D 3S AC', '8C AD 8D AC 9C'
   /***************************************************
                      Finding Rank
   ****************************************************/
-  //isFlush
-  function isFlush( arr ) {
-    flush = false
-    arr.forEach( function ( suit, pos ) {
-      if ( pos === arr.length - 1 ) {} else {
-        arr[ pos ] === arr[ pos + 1 ] ? flush = true : flush = false;
-      }
-    } );
-    return flush;
-  }
-  //isStraight
-  //findMatches looks at the sorted emnumerated values and then determines how many matches there are
-  function findMatches( arr ) {
-    var matches = 0,
-      lastmatch = 0,
-      match = arr[ lastmatch ];
 
-    for ( i = 0; i < arr.length; i++ ) {
-      arr[ i ] === match ? matches += 1 : match = arr[ i ];
+  function getHand( suits, values ) {
+    var hand = {};
+    isFlush( suits );
+    //isFlush
+    function isFlush( arr ) {
+      arr.forEach( function ( suit, pos ) {
+        if ( pos === arr.length - 1 ) {} else {
+          arr[ pos ] === arr[ pos + 1 ] ? hand.flush = true : hand.flush = false;
+        }
+      } );
+      isStraight( values );
     }
-    if ( matches > 4 ) {
-      matches += 1;
-    } // the add one accounts for the switch when a new match is found
+    //isStraight
+    function isStraight( arr ) {
+      hand.straight = ( arr[ 0 ] + 1 == arr[ 1 ] || ( arr[ 0 ] == 1 && arr[ 4 ] == 13 ) ) &&
+        ( arr[ 1 ] + 1 == arr[ 2 ] ) &&
+        ( arr[ 2 ] + 1 == arr[ 3 ] ) &&
+        ( arr[ 3 ] + 1 == arr[ 4 ] );
+      findMatches( values );
+    }
+    //findMatches looks at the sorted emnumerated values and then determines how many matches there are
+    function findMatches( arr ) {
+      var matches = 0,
+        last_matches = 0,
+        first_matches = 0,
+        diffHand = false,
+        match = arr[ 0 ];
 
-    return matches;
+      arr = arr.sort( ( a, b ) => a - b ); // this is unnecessary given the data but makes the code reuseable
+
+      for ( i = 0; i < arr.length; i++ ) {
+        if ( arr[ i ] === match ) {
+          matches += 1;
+          if ( diffHand ) {
+            last_matches += 1;
+          } else {
+            first_matches += 1;
+          }
+        } else {
+          match = arr[ i ], diffHand = true;
+        }
+      }
+      if ( matches === 4 && ( first_matches != 4 && last_matches != 3 ) ) {
+        matches += 1;
+      }
+      hand.diffHand = diffHand;
+      //  NOTE: the addition of one accounts for the switch when a new match is found on a two pair
+      // last_matches != 3 works as the last int in the array does not get logged as a match.
+      // the array must be sorted for this to work.
+      hand.matches = matches;
+      findHighCard( values );
+    }
+
+    function findHighCard( arr ) {
+      hand.highCard = arr[ arr.length - 1 ];
+    }
+    handData.push( hand );
   }
+
 }( hands ) );

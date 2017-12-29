ecf0d5647596c0eab4cb7407cd446874c6c2128c
Adam <adam@adamharpur.com>
2017-12-29 13:11:13 -0600

    fix(typo)

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ var config = {
 var ranks = {
   0: "Five of a kind", // incase wild jokers are added
   1: "Royal flush",
-  2: "Staight flush",
+  2: "Straight flush",
   3: "Four of a kind",
   4: "Full House",
   5: "Flush",
